ddc9f19ec8202c63d3012045e43f4358973152eb
Timothy Karani <kartimothy@gmail.com>
2021-09-10 07:48:21 +0300

    Can create new user

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/test', function () {
   return ["message" => "hello world"];
 });
+
+Route::post('auth/register', [UserController::class, 'store'])->name('auth.register');
