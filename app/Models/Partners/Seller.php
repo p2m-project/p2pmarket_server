86a193aca7090c085bd3ec96d00945faf72fa29b
Timothy Karani <kartimothy@gmail.com>
2021-09-28 00:13:57 +0300

    Added seller migration

---
+++ app/Models/Partners/Seller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Partners;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Seller extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_09_27_211147_create_sellers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSellersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('username');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('password');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sellers');
+    }
+}
