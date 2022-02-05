d5a15eee72f4e35e0b5f3c840a424a6d08582b73
Timothy Karani <kartimothy@gmail.com>
2022-02-06 02:13:16 +0300

    Added product pricings migration

---
+++ database/migrations/2022_02_05_230750_create_product_pricings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductPricingsTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('product_pricings', function (Blueprint $table) {
+      $table->id();
+      $table->unsignedDecimal('price', 12, 2);
+      $table->unsignedBigInteger('product_id');
+      $table->string('upc');  // universal product code
+      $table->string('sku');  // stock keeping unit
+      $table->softDeletes();
+      $table->timestamps();
+
+      $table->foreign('product_id')->references('id')->on('products');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('product_pricings');
+  }
+}
