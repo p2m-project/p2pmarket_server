9373ebca0da9023c615e4f0c7d5c6a6755ded676
Timothy Karani <kartimothy@gmail.com>
2022-02-11 23:14:18 +0300

    Added get endpoints

---
+++ app/Http/Controllers/Products/ProductImageController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Products;
+
+use App\Http\Controllers\ApiController;
+use App\Models\Products\ProductImage;
+use Illuminate\Http\Request;
+
+class ProductImageController extends ApiController
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $productImages = ProductImage::all();
+    return $this->showAll($productImages);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    //
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\Products\ProductImage  $productImage
+   * @return \Illuminate\Http\Response
+   */
+  public function show(ProductImage $productImage)
+  {
+    return $this->showOne($productImage);
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Models\Products\ProductImage  $productImage
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(ProductImage $productImage)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Models\Products\ProductImage  $productImage
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, ProductImage $productImage)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Products\ProductImage  $productImage
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(ProductImage $productImage)
+  {
+    //
+  }
+}
